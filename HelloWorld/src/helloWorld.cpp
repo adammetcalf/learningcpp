5aa117dda4d61ac87f9384df383d65bc8b35f62a
Adam Metcalf <mn14agm@leeds.ac.uk>
2021-06-17 10:06:34 +0100

    setup solution and Hello world project

---
+++ HelloWorld/src/helloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>														//standard library for inputs/outputs
+
+
+int main()																//entry point
+{
+	std::cout << "Hello, world!" << std::endl;							//outputting the message and an endline
+	std::cin.get();														//await enter being pressed before closing command window
+}
